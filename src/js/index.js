735b93c6a76516eeab904620d33d6ba4032fdb11
Emily Vong <emily.vong25@gmail.com>
2018-06-08 23:17:41 +0000

    initial commit containing html, css and js with vanillajs

---
+++ src/js/index.js
@@ -0,0 +1,36 @@
+/*
+    This is your main stylesheet, you should use it only
+    to include other styles into your web app.
+*/
+//include jquery into the bundle and store its contents into the $ variable
+import $ from "jquery";
+//include bootstrap npm library into the bundle
+import 'bootstrap';
+//include your own styles
+import '../style/index.scss';
+
+window.onload = function(){
+    var value = ["2","3","4","5","6","7","8","9","10","J","Q","K","A"];
+    var suits = ["&heartsuit;" , "&diamondsuit;", "&clubs;", "&spadesuit;"];
+    
+    var num = Math.floor(Math.random() * value.length);
+    var randomValue = value[num];
+    
+    var suit = Math.floor(Math.random() * suits.length);
+    var randomSuit = suits[suit];
+    
+    
+    document.getElementById('cardValue').innerHTML=randomValue;
+    
+    document.getElementById('topSuit').innerHTML = randomSuit;
+    
+    document.getElementById('bottomSuit').innerHTML = randomSuit;
+    
+    
+    if (randomSuit === "&heartsuit;" || randomSuit === "&diamondsuit;"){
+      document.getElementById('topSuit').className="";
+      document.getElementById('bottomSuit').className="";
+      document.getElementById('topSuit').classList.add('turnRed');
+      document.getElementById('bottomSuit').classList.add('turnRed');
+    }
+};
