76b29921c2a818abe737174653bf4514580fed2b
Emily Vong <emily.vong25@gmail.com>
2018-06-09 00:45:29 +0000

    added button with refresh

--- src/js/index.js
+++ src/js/index.js
@@ -10,27 +10,40 @@ import 'bootstrap';
 import '../style/index.scss';
 
 window.onload = function(){
-    var value = ["2","3","4","5","6","7","8","9","10","J","Q","K","A"];
-    var suits = ["&heartsuit;" , "&diamondsuit;", "&clubs;", "&spadesuit;"];
+
     
-    var num = Math.floor(Math.random() * value.length);
-    var randomValue = value[num];
+   
     
-    var suit = Math.floor(Math.random() * suits.length);
-    var randomSuit = suits[suit];
+
+
+
+        var value = ["2","3","4","5","6","7","8","9","10","J","Q","K","A"];
+        var suits = ["&heartsuit;" , "&diamondsuit;", "&clubs;", "&spadesuit;"];
+        
+        var num = Math.floor(Math.random() * value.length);
+        var randomValue = value[num];
+        
+        var suit = Math.floor(Math.random() * suits.length);
+        var randomSuit = suits[suit];
+        
+        if (randomSuit === "&heartsuit;" || randomSuit === "&diamondsuit;"){
+          document.getElementById('topSuit').className="";
+          document.getElementById('bottomSuit').className="";
+          document.getElementById('topSuit').classList.add('turnRed');
+          document.getElementById('bottomSuit').classList.add('turnRed');
+        }
+        
+        document.getElementById('cardValue').innerHTML=randomValue;
+        
+        document.getElementById('topSuit').innerHTML = randomSuit;
+        
+        document.getElementById('bottomSuit').innerHTML = randomSuit;
+        
+    
+    //Reloads whole page on button click
+    window.newCard = function(){
+        window.location.reload();
+    };
     
     
-    document.getElementById('cardValue').innerHTML=randomValue;
-    
-    document.getElementById('topSuit').innerHTML = randomSuit;
-    
-    document.getElementById('bottomSuit').innerHTML = randomSuit;
-    
-    
-    if (randomSuit === "&heartsuit;" || randomSuit === "&diamondsuit;"){
-      document.getElementById('topSuit').className="";
-      document.getElementById('bottomSuit').className="";
-      document.getElementById('topSuit').classList.add('turnRed');
-      document.getElementById('bottomSuit').classList.add('turnRed');
-    }
 };
